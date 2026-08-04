b135ddb3b17ea5138c873b9c66dc01b2ec0e8d1f
AndreasDebel <andreasdebel@hotmail.com>
2026-08-04 20:04:46 +0200

    Revert "test: turn home-page text yellow as a deploy smoke test"
    This reverts commit 40c1bb668b54631d03d5fb9eb2e22a8004340e72.

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -5,8 +5,6 @@ import { buildMetadata } from '@/components/seo/metadata'
 import { client } from '@/sanity/client'
 import { HOME_PAGE_QUERY } from '@/sanity/queries/home'
 
-import styles from './home-color-test.module.css'
-
 async function fetchHomePage() {
   return client.fetch(HOME_PAGE_QUERY, {}, { next: { tags: ['homePage'] } })
 }
@@ -36,9 +34,5 @@ export default async function HomePage() {
     )
   }
 
-  return (
-    <div className={styles.yellowTest}>
-      <BlockRenderer blocks={data.blocks} />
-    </div>
-  )
+  return <BlockRenderer blocks={data.blocks} />
 }
